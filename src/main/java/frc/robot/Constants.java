29ee5862abfa6d368f31177b099974a788728105
Connor2039 <connoros349@gmail.com>
2024-02-28 00:05:35 -0600

    swerve running :D

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -59,10 +59,10 @@ public final class Constants {
         new Translation2d(-kWheelBase / 2, -kTrackWidth / 2));
 
     // Angular offsets of the modules relative to the chassis in radians
-    public static final double kFrontLeftChassisAngularOffset = 0; //-Math.PI / 2;
-    public static final double kFrontRightChassisAngularOffset = -Math.PI / 2; //0
-    public static final double kBackLeftChassisAngularOffset =  -Math.PI / 2; //Math.PI;
-    public static final double kBackRightChassisAngularOffset = 0; //Math.PI / 2;
+    public static final double kFrontLeftChassisAngularOffset = Math.PI / 2; //-Math.PI / 2;
+    public static final double kFrontRightChassisAngularOffset = 0; //0
+    public static final double kBackLeftChassisAngularOffset =  0; //Math.PI;
+    public static final double kBackRightChassisAngularOffset = Math.PI / 2; //Math.PI / 2;
 
     // SPARK MAX CAN IDs
     public static final int kFrontLeftDrivingCanId = 13;
